0641ea07b2e7a6ea8d9e76e11f757e89f493ae48
shahon922 <120345796+shahon922@users.noreply.github.com>
2024-02-01 09:09:45 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.25 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        for(int i = 0; i < 10; i++)
+        {
+            answer += i;
+        }
+        
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            answer -= numbers[i];
+        }
+        
+        return answer;
+    }
+}
